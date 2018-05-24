102ed0befafc7d53a4c9a5c7a01ba03487c89cb7
Will T <toherwils@gmail.com>
2018-05-24 16:12:13 -0500

    Working on bus refactor

--- server.js
+++ server.js
@@ -36,37 +36,95 @@ let trainStations = ["40350"];
 let location = "Chicago";
 
 function getBus() {
-	// for (let stop of busStops) {
-	// 	getFromUrl("http://ctabustracker.com/bustime/api/v2/getpredictions?key="+busApiKey+"&stpid="+stop+"&format=json")
-	// 		.then((result) => {
+	let busUrl = "http://ctabustracker.com/bustime/api/v2/getpredictions?format=json&key=" + busApiKey + "&stpid=";
+	let busPromises = [];
 
-	// 		});
-	// }
-	for (let i=0;i<busStops.length;i++) {
-		let busUrl="http://ctabustracker.com/bustime/api/v2/getpredictions?key="+busApiKey+"&stpid="+busStops[i]+"&format=json";
-		let busExport = [];
-		getFromUrl(busUrl).then((result) => {
+	// API can accept up to 10 stops per call; want to minimize calls
+	for (let i = 0; (10 + i) < busStops.length; i += 10) {
+
+		// Array.slice returns a shallow copy into region of array
+		// Doesn't matter if (i+10) > busStops.length
+		let stops = busStops.slice(i, i + 10);
+		let stopsString = stops.join(",");
+
+		busPromises.push(getFromUrl(busUrl + stopsString).then((result) => {
+			let busTimes = {};
 			let busJson = JSON.parse(result)["bustime-response"];
-			//console.log(busJson);
 			if (busJson.hasOwnProperty("error")) {
 				// TODO
-				console.log("Error response on bus " + busStops[i]);
+				console.log("Error response on busses " + stopsString);
 				return;
 			}
-			for (let j=0;j<busJson["prd"].length;j++) {
-				let data = {
-					stop: busJson["prd"][j]["rt"],
-					route: busJson["prd"][j]["stpnm"],
-					dir: busJson["prd"][j]["rtdir"], 
-					prdtm: busJson["prd"][j]["prdtm"].split(" ")[1],
-					tmstmp: busJson["prd"][j]["tmstmp"].split(" ")[1],
-				};
-				busExport.push(data);
+
+			// Iterate through all returned predictions
+			for (let prd of busJson["prd"]) {
+				// Route name is number + 1st letter of direction
+				// e.g. 12 Eastbound would be "12E"
+				let routeName = prd.rtdd + prd.rtdir.charAt(0);
+				if (!busTimes.hasOwnProperty(routeName)) {
+					busTimes[routeName] = [];
+				}
+				busTimes[routeName].push(prd);
 			}
-			o[busKey].push(busExport);
-        
-		});
+
+			// Now sort all returned predictions by soonest
+			for (let prds in busTimes) {
+				prds.sort((a, b) => {
+					if (a.prdctdn === b.prdctdn) {
+						return 0;
+					}
+					if (a.prdctdn === "DUE") {
+						return -1;
+					}
+					if (b.prdctdn === "DUE") {
+						return 1;
+					}
+					// parseInt() technically not needed due to type coersion, but it's probably a bad idea to trust type coersion
+					return parseInt(a.prdctdn, 10) - parseInt(b.prdctdn, 10);
+				});
+			}
+			return busTimes;
+		}));
+
 	}
+	// Remember that getFromUrl runs asynchronously
+	// In cases where there are more than 10 stops being examined, we need
+	// to wait for all requests to finish, then merge resulting objects together
+
+	// !!! Assumption: no more than one bus stop per route name !!!
+	// If this is false, below code will copy over some values 
+
+	return Promise.all(busPromises).then((allBusTimes) => {
+		let returnTimes = {};
+		for (let bt of allBusTimes) {
+			Object.assign(returnTimes, bt);
+		}
+		return returnTimes;
+	});
+	
+	// This function now returns a Promise object
+
+
+	// for (let i=0;i<busStops.length;i++) {
+	// 	let busUrl="http://ctabustracker.com/bustime/api/v2/getpredictions?key="+busApiKey+"&stpid="+busStops[i]+"&format=json";
+	// 	let busExport = [];
+	// 	getFromUrl(busUrl).then((result) => {
+	// 		let busJson = JSON.parse(result)["bustime-response"];
+	// 		//console.log(busJson);
+	// 		for (let j=0;j<busJson["prd"].length;j++) {
+	// 			let data = {
+	// 				stop: busJson["prd"][j]["rt"],
+	// 				route: busJson["prd"][j]["stpnm"],
+	// 				dir: busJson["prd"][j]["rtdir"], 
+	// 				prdtm: busJson["prd"][j]["prdtm"].split(" ")[1],
+	// 				tmstmp: busJson["prd"][j]["tmstmp"].split(" ")[1],
+	// 			};
+	// 			busExport.push(data);
+	// 		}
+	// 		o[busKey].push(busExport);
+        
+	// 	});
+	// }
 }
 
 function getTrain() {
@@ -144,7 +202,7 @@ function getData() {
 		o[trainKey] = [];
 		o[weatherKey] = [];
 		getData();
-	}, 15000);
+	}, 10000);
 }
 
 getData();
